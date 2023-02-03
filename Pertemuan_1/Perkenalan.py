8b8cca926fa8229221775e48293c7f0fa309181f
bagusasus <bagusa.fadhilah@gmail.com>
2023-02-03 22:43:17 +0700

    Testing With Tea

---
+++ Pertemuan_1/Perkenalan.py
@@ -0,0 +1,14 @@
+X = (int(input("Masukan Umur         : ")))
+Y = (input("Masukan Nama         : "))
+Z = (input("Masukan Asal Sekolah : "))
+XY = (float(input("Masukan Berat Badan  : ")))
+
+if Z == "SMK":
+    a = True
+else:
+    a = False
+
+print(f"Umur             : {X}")
+print(f"Nama             : {Y}")
+print(f"Asal Sekolah SMK : {Z}")
+print(f"Berat Badan      : {XY}")
